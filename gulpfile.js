4b5e552f22d64946d67b2409a677d69d86c926c6
BOSTON\chaitra.naik <chaitra.naik@ef.com>
2017-09-28 12:56:28 +0530

    initial commit

---
+++ gulpfile.js
@@ -0,0 +1,76 @@
+var gulp = require('gulp');
+var spritesmith  = require('gulp.spritesmith');
+var runSequence = require('run-sequence');
+var concat = require('gulp-concat');
+var uglify = require('gulp-uglify');
+var rename = require('gulp-rename');
+var sass = require('gulp-sass');
+var cleanCSS = require('gulp-clean-css');
+var browserSync = require('browser-sync').create();
+var svgSprite = require("gulp-svg-sprites");
+
+gulp.task('sprite', function () {
+    return gulp.src('src/dist/images/icons/*.svg')
+        .pipe(svgSprite())
+        .pipe(gulp.dest("src/dist/sprite"));
+});
+
+gulp.task('scripts', function() {
+  gulp.src('src/scripts/*.*')
+    .pipe(concat('app.js'))
+    .pipe(uglify())
+    .pipe(gulp.dest('src/dist/scripts/'))
+    .pipe(rename({ suffix: '.min' }))
+    .pipe(gulp.dest('src/dist/scripts/'))
+});
+
+gulp.task('vendorScripts', function() {
+  gulp.src([
+      'src/vendor/jquery.min.js',
+      'src/vendor/jquery-ui.js',
+      'src/vendor/slick.js',
+      'src/vendor/select2.full.js',
+      'src/vendor/jquery.inview.js',
+      'src/vendor/jquery.youtube-inview-autoplay.js'
+    ])
+    .pipe(concat('vendor.js'))
+    .pipe(uglify())
+    .pipe(gulp.dest('src/dist/scripts/'))
+    .pipe(rename({ suffix: '.min' }))
+    .pipe(gulp.dest('src/dist/scripts/'))
+});
+
+gulp.task('vendorStyles', function() {
+  gulp.src('src/vendor/*.css')
+    .pipe(concat('vendor.css'))
+    .pipe(gulp.dest('src/dist/styles/'))
+    .pipe(rename({ suffix: '.min' }))
+    .pipe(gulp.dest('src/dist/styles/'))
+});
+
+gulp.task('styles', function () {
+  return gulp.src('src/styles/main.scss')
+  .pipe(sass().on('error', sass.logError))
+  .pipe(rename('style.css'))
+  .pipe(gulp.dest('src/dist/styles'))
+  .pipe(cleanCSS({compatibility: 'ie8'}))
+  .pipe(rename({ suffix: '.min' }))
+  .pipe(gulp.dest('src/dist/styles'));
+});
+
+gulp.task('serve', function() {
+  browserSync.init({
+    server: {
+      baseDir: 'src'
+    },
+  })
+})
+
+gulp.task('default', function (callback) {
+    runSequence('sprite', 'scripts', 'styles', 'vendorScripts', 'vendorStyles',  callback);
+});
+
+gulp.task('watch', function() {
+    gulp.watch('src/styles/**/*.scss', ['styles'])
+    gulp.watch('src/scripts/*.*', ['scripts'])
+});
---
+++ src/data/data-en.js
@@ -0,0 +1,577 @@
+(function(window){
+	
+	'use strict';
+
+	window.cityHotelMap = {
+		'tokyo': {
+			city: 'Tokyo',
+			id: 'tokyo',
+			hotels: [
+				{
+					name: 'HOTEL MYSTAYS PREMIER Akasaka',
+					address: '2-17-54 Akasaka, Minato-ku, Tokyo 107-0052 Japan',
+					img: 'HOTEL-MYSTAYS-PREMIER-Akasaka.jpg',
+					id: 'jptok26324'
+				},
+				{
+					name: 'HOTEL MYSTAYS PREMIER Hamamatsucho',
+					address: '1-8-5 Hamamatsucho, Minato-ku, Tokyo 105-0013 Japan',
+					img: 'HOTEL-MYSTAYS-PREMIER-Hamamatsucho.jpg',
+					id: 'jptok28530'
+				},
+				{
+					name: 'HOTEL MYSTAYS PREMIER Omori',
+					address: '6-19-3 Minami Oi, Shinagawa-ku, Tokyo 140-0013 Japan',
+					img: 'HOTEL-MYSTAYS-PREMIER-Omori.jpg',
+					id: 'jptok28531'
+				},
+				{
+					name: 'HOTEL MYSTAYS Asakusa',
+					address: '1-21-11 Honjo, Sumida-ku, Tokyo 130-0004 Japan',
+					img: 'HOTEL-MYSTAYS-Asakusa.jpg',
+					id: 'jptok26318'
+				},
+				{
+					name: 'HOTEL MYSTAYS Asakusa bashi',
+					address: '1-5-5 Asakusabashi, Taito-ku, Tokyo 111-0053 Japan',
+					img: 'HOTEL-MYSTAYS-Asakusabashi.JPG',
+					id: 'jptok26316'
+				},
+				{
+					name: 'HOTEL MYSTAYS Gotanda',
+					address: '2-5-4 Higashigotanda, Shinagawa-ku, Tokyo 141-0022 Japan',
+					img: 'HOTEL-MYSTAYS-Gotanda.jpg',
+					id: 'jptok26323'
+				},
+				{
+					name: 'HOTEL MYSTAYS Gotanda Station',
+					address: '2-6-8 Nishigotanda, Shinagawa-ku, Tokyo 141-0031 Japan',
+					img: 'HOTEL-MYSTAYS-Gotanda-Station.jpg',
+					id: 'jptok26951'
+				},
+				{
+					name: 'HOTEL MYSTAYS Hamamatsucho',
+					address: '1-18-14 Hamamatsucho, Minato-ku, Tokyo 105-0013 Japan',
+					img: 'HOTEL-MYSTAYS-Hamamatsucho.jpg',
+					id: 'jptok26322'
+				},
+				{
+					name: 'HOTEL MYSTAYS Haneda',
+					address: '5-1-13 Haneda, Ota-ku, Tokyo 144-0043 Japan',
+					img: 'HOTEL-MYSTAYS-Haneda.jpg',
+					id: 'jptok26798'
+				},
+				{
+					name: 'HOTEL MYSTAYS Higashi Ikebukuroa',
+					address: '4-39-13 Higashiikebukuro, Toshima-ku, Tokyo 170-0013 Japan',
+					img: 'HOTEL-MYSTAYS-Higashi-Ikebukuro.jpg',
+					id: 'jptok26319'
+				},
+				{
+					name: 'HOTEL MYSTAYS Kamata',
+					address: '5-46-5 Kamata, Ota-ku, Tokyo 144-0052 Japan n',
+					img: 'HOTEL-MYSTAYS-Kamata.JPG',
+					id: 'jptok26325'
+				},
+				{
+					name: 'HOTEL MYSTAYS Kameido',
+					address: '6-32-1 Kameido, Koto-ku, Tokyo 136-0071 Japan',
+					img: 'HOTEL-MYSTAYS-Kameido.JPG',
+					id: 'jptok26311'
+				},
+				{
+					name: 'HOTEL MYSTAYS Kanda',
+					address: '1-2-2 Iwamotocho Chiyoda-ku, Tokyo 101-0032 Japan',
+					img: 'HOTEL-MYSTAYS-Kanda.jpg',
+					id: 'jptok26313'
+				},
+				{
+					name: 'HOTEL MYSTAYS Nippori',
+					address: '5-43-7 Higashi nippori, Arakawa-ku, Tokyo 116-0014 Japan',
+					img: 'HOTEL-MYSTAYS-Nippori.jpg',
+					id: 'jptok26329'
+				},
+				{
+					name: 'HOTEL MYSTAYS Nishi Shinjuku',
+					address: '7-14-14 Nishishinjuku, Shinjuku-ku, Tokyo 160-0023 Japan',
+					img: 'HOTEL-MYSTAYS-Nishi-Shinjuku.jpg',
+					id: 'jptok26320'
+				},
+				{
+					name: 'HOTEL MYSTAYS Ochanomizu Conference Center',
+					address: '2-10-6 Kandaawajicho, Chiyoda-ku, Tokyo 101-0063 Japan',
+					img: 'HOTEL-MYSTAYS-Ochanomizu-Conference-Center.jpg',
+					id: 'jptok26310'
+				},
+				{
+					name: 'HOTEL MYSTAYS Ueno East',
+					address: '5-5-6 Higashi Ueno, Taito-ku, Tokyo 110-0015 Japan',
+					img: 'HOTEL-MYSTAYS-Ueno-East.jpg',
+					id: 'jptok28305'
+				},
+				{
+					name: 'HOTEL MYSTAYS Ueno Inaricho',
+					address: '1-5-7 Matsugaya, Taito-ku, Tokyo 111-0036 Japan',
+					img: 'HOTEL-MYSTAYS-Ueno-Inaricho.jpg',
+					id: 'jptok26312'
+				},
+				{
+					name: 'HOTEL MYSTAYS Ueno Iriyaguchi',
+					address: '5-5-13 Higashiueno, Taito-ku, Tokyo 110-0015 Japan',
+					img: 'HOTEL-MYSTAYS-Ueno-Iriyaguchi.jpg',
+					id: 'jptok26350'
+				},
+				{
+					name: 'FLEXSTAY INN Ekoda',
+					address: '8-6 Sakaecho, Nerima-ku,Tokyo 176-0006 Japan',
+					img: 'FLEXSTAY-INN-Ekoda.JPG',
+					id: 'jptok26345'
+				},
+				{
+					name: 'FLEXSTAY INN Higashi Jujo',
+					address: '2-10-2 Nakajujo, Kita-ku, Tokyo 114-0032 Japan',
+					img: 'FLEXSTAY-INN-Higashi-Jujo.JPG',
+					id: 'jptok26339'
+				},
+				{
+					name: 'FLEXSTAY INN Iidabashi',
+					address: '3-26 Shinogawamachi, Shinjuku-ku, Tokyo 162-0814 Japan',
+					img: 'FLEXSTAY-INN-Iidabashi.JPG',
+					id: 'jptok26340'
+				},
+				{
+					name: 'FLEXSTAY INN Kiyosumi Shirakawa',
+					address: '1-12-16 Tokiwa, Koto-ku, Tokyo 135-0006 Japan',
+					img: 'FLEXSTAY-INN-Kiyosumi-Shirakawa.JPG',
+					id: 'jptok26348'
+				},
+				{
+					name: 'FLEXSTAY INN Nakanobu',
+					address: '4-27-8 Futaba, Shinagawa-ku, Tokyo 142-0043 Japan',
+					img: 'FLEXSTAY-INN-Nakanobu.JPG',
+					id: 'jptok26346'
+				},
+				{
+					name: 'FLEXSTAY INN Shinagawa',
+					address: '1-22-19 Kitashinagawa, Shinagawa-ku, Tokyo 140-0001 Japan',
+					img: 'FLEXSTAY-INN-Shinagawa.JPG',
+					id: 'jptok26341'
+				},
+				{
+					name: 'FLEXSTAY INN Shirogane',
+					address: '5-10-15 Shirogane, Minato-ku, Tokyo 108-0072 Japan',
+					img: 'FLEXSTAY-INN-Shirogane.jpg',
+					id: 'jptok26336'
+				},
+				{
+					name: 'FLEXSTAY INN Sugamo',
+					address: '3-6-16 Sugamo, Toshima-ku, Tokyo 170-0002 Japan',
+					img: 'FLEXSTAY-INN-Sugamo.JPG',
+					id: 'jptok26333'
+				},
+				{
+					name: 'FLEXSTAY INN Tokiwadai',
+					address: '1-52-5 Tokiwadai, Itabashi-ku, Tokyo 174-0071 Japan',
+					img: 'FLEXSTAY-INN-Tokiwadai.JPG',
+					id: 'jptok26344'
+				},
+				{
+					name: 'MyCUBE by MYSTAYS Asakusa Kuramae',
+					address: '2-6-7 Kuramae, Taito-ku, Tokyo 111-0051 Japan',
+					img: 'MyCUBE-by-MYSTAYS-Asakusa-Kuramae.jpg',
+					id: 'jptok28108'
+				}
+			]
+		},
+		'kyoto':{
+			city: 'Kyoto',
+			id: 'kyoto',
+			hotels: [
+				{
+					name: 'HOTEL MYSTAYS Kyoto Shijo',
+					address: '52 Kasabokocho, Aburanokoji Higashiiru, Shijo-dori, Shimogyo-ku, Kyoto-shi, Kyoto',
+					img: 'HOTEL-MYSTAYS-Kyoto-Shijo.jpg',
+					id: 'jpkyo26330'
+				}
+			]
+		},
+		'osaka':{
+			city: 'Osaka',
+			id: 'osaka',
+			hotels: [
+				{
+					name: 'HOTEL MYSTAYS Midosuji Honmachi',
+					address: '3-2-11, Hommachi, Chuo-ku, Osaka-shi, Osaka, 541-0053, Japan',
+					img: 'HOTEL-MYSTAYS-Midosuji-Honmachi.jpg',
+					id: 'jposa31260'
+				},
+				{
+					name: 'HOTEL MYSTAYS Otemae',
+					address: '1-3-2 Tokuicho, Chuo-ku, Osaka-shi, Osaka 540-0025 Japan',
+					img: 'HOTEL-MYSTAYS-Otemae.JPG',
+					id: 'jposa26328'
+				},
+				{
+					name: 'HOTEL MYSTAYS Sakaisuji Honmachi',
+					address: '1- 4-8 Awajimachi, Chuo-ku, Osaka-shi, Osaka 541-0047 Japan',
+					img: 'HOTEL-MYSTAYS-Sakaisuji-Honmachi.JPG',
+					id: 'jposa26315'
+				},
+				{
+					name: 'HOTEL MYSTAYS Shinsaibashi',
+					address: '1-9-30 Nishishinsaibashi, Chuo-ku, Osaka-shi, Osaka 542-0086 Japan',
+					img: 'HOTEL-MYSTAYS-Shinsaibashi.jpg',
+					id: 'jposa26338'
+				},
+				{
+					name: 'HOTEL MYSTAYS Shinsaibashi East',
+					address: '2-6-4 Shimanouchi, Chuo-ku, Osaka-shi, Osaka 542-0082 Japan',
+					img: 'HOTEL-MYSTAYS-Shinsaibashi-East.jpg',
+					id: 'jposa28306'
+				},
+				{
+					name: 'HOTEL MYSTAYS Shin Osaka Conference Center',
+					address: '6-2-19 Nishinakajima, Yodogawa-ku, Osaka-shi, Osaka 532-0011 Japan',
+					img: 'HOTEL-MYSTAYS-Shin-Osaka-Conference-Center.jpg',
+					id: 'jposa28304'
+				}
+			]
+		},
+		'kanazawa':{
+			city: 'Kanazawa',
+			id: 'kanazawa',
+			hotels: [
+				{
+					name: 'HOTEL MYSTAYS PREMIER Kanazawa',
+					address: '2-13-5 Hirooka, Kanazawa-shi, Ishikawa 920-0031 Japan',
+					img: 'HOTEL-MYSTAYS-PREMIER-Kanazawa.jpg',
+					id: 'jpkan26799'
+				},
+				{
+					name: 'HOTEL MYSTAYS Kanazawa Castle',
+					address: '10-17 Konohana-machi, Kanazawa-shi, Ishikawa 920-0852 Japan',
+					img: 'HOTEL-MYSTAYS-Kanazawa-Castle.jpg',
+					id: 'jpkan27429'
+				}
+			]
+		},
+		'maihama-urayasu':{
+			city: 'Maihama-urayasu',
+			id: 'maihama-urayasu',
+			hotels: [
+				{
+					name: 'HOTEL MYSTAYS Maihama',
+					address: '3-5-1 Tekkodori, Urayasu-shi, Chiba 279-0025 Japan',
+					img: 'HOTEL-MYSTAYS-Maihama.jpg',
+					id: 'jpura26317'
+				},
+				{
+					name: 'MYSTAYS Shin Urayasu Conference Center',
+					address: '2-1-4 Akemi, Urayasu-shi, Chiba 279-0014 Japan',
+					img: 'MYSTAYS-Shin-Urayasu-Conference-Center.jpg',
+					id: 'jpura26309'
+				},
+				{
+					name: 'FLEXSTAY INN Shin Urayasu',
+					address: '4-1-29 Imagawa, Urayasu-shi, Chiba 279-0022 Japan',
+					img: 'FLEXSTAY-INN-Shin-Urayasu.jpg',
+					id: 'jpura26342'
+				}
+			]
+		},
+		'tachikawa':{
+			city: 'Tachikawa',
+			id: 'tachikawa',
+			hotels: [
+				{
+					name: 'HOTEL MYSTAYS Tachikawa',
+					address: '1-8-14 Nishiki-cho, Tachikawa-shi, Tokyo 190-0022 Japan',
+					img: 'HOTEL-MYSTAYS-Tachikawa.jpg',
+					id: 'jptok27899'
+				}
+			]
+		},
+		'kawasaki':{
+			city: 'Kawasaki',
+			id: 'kawasaki',
+			hotels: [
+				{
+					name: 'FLEXSTAY INN Kawasaki Kaizuka',
+					address: '1-13-2 Kaizuka, Kawasaki-ku, Kawasaki-shi, Kanagawa 210-0014 Japan',
+					img: 'FLEXSTAY-INN-Kawasaki-Kaizuka.jpg',
+					id: 'jpkaw26347'
+				},
+				{
+					name: 'FLEXSTAY INN Kawasaki Ogawacho',
+					address: '15-9 Ogawacho, Kawasaki-ku, Kawasaki-shi, Kanagawa 210-0023 Japan',
+					img: 'FLEXSTAY-INN-Ogawacho.JPG',
+					id: 'jpkaw26337'
+				},
+				{
+					name: 'FLEXSTAY INN Tamagawa',
+					address: '1-837 Shinmaruko-Higashi, Nakahara-ku, Kawasaki-shi, Kanagawa 211-0004 Japan',
+					img: 'FLEXSTAY-INN-Tamagawa.JPG',
+					id: 'jpkaw26335'
+				}
+			]
+		},
+		'yokohama':{
+			city: 'Yokohama',
+			id: 'yokohama',
+			hotels: [
+				{
+					name: 'HOTEL MYSTAYS Yokohama',
+					address: '4-81 Sueyoshicho, Naka-ku, Yokohama-shi, Kanagawa 231-0055 Japan',
+					img: 'HOTEL-MYSTAYS-Yokohama.JPG',
+					id: 'jpyok26332'
+				},
+				{
+					name: 'HOTEL MYSTAYS Yokohama Kannai',
+					address: '5-67-1 Onoe-cho, Naka-ku, Yokohama-shi, Kanagawa 231-0015 Japan',
+					img: 'HOTEL-MYSTAYS-Yokohama-Kannai.jpg',
+					id: 'jpyok29010'
+				},
+				{
+					name: 'FLEXSTAY INN Sakuragicho',
+					address: '2-72-16 Hanasaki-cho, Naka-ku, Yokohama-shi, Kanagawa 231-0063 Japan',
+					img: 'FLEXSTAY-INN-Sakuragicho.jpg',
+					id: 'jpyok26343'
+				}
+			]
+		},
+		'chiba':{
+			city: 'Chiba',
+			id: 'chiba',
+			hotels: [
+				{
+					name: 'Shirahama Ocean Resort',
+					address: '2784 Shirahama-cho Minamiboso-shi, Chiba, 295-0102, Japan',
+					img: 'Shirahama-Ocean-Resort.jpg',
+					id: 'jpchi28828'
+				}
+			]
+		},
+		'tochigi':{
+			city: 'Tochigi',
+			id: 'tochigi',
+			hotels: [
+				{
+					name: 'HOTEL MYSTAYS Utsunomiya',
+					address: '2-4-1 Higashishukugo, Utsunomiya-shi, Tochigi 321-0953 Japan',
+					img: 'HOTEL-MYSTAYS-Utsunomiya.JPG',
+					id: 'jputs26331'
+				},
+				{
+					name: 'Hotel Épinard Nasu',
+					address: '1 Ooaza-Takakuhei, Nasu-machi, Nasu-gun, Tochigi 325-0302 Japan',
+					img: 'Hotel-epinard-Nasu.jpg',
+					id: 'jpnas28830'
+				}
+			]
+		},
+		'hokkaido':{
+			city: 'Hokkaido',
+			id: 'hokkaido',
+			hotels: [
+				{
+					name: 'HOTEL MYSTAYS PREMIER Sapporo Park',
+					address: '2-10 Nishi 2-chome, Minami 9-jo, Chuo-ku, Sapporo-shi, Hokkaido 064-0809 Japan',
+					img: 'HOTEL-MYSTAYS-PREMIER-Sapporo-Park.jpg',
+					id: 'jphok28823'
+				},
+				{
+					name: 'HOTEL MYSTAYS Sapporo Aspen',
+					address: '5 Nishi 4-chome, Kita 8-jo, Kita-ku, Sapporo-shi, Hokkaido 060-0808 Japan',
+					img: 'HOTEL-MYSTAYS-Sapporo-Aspen.jpg',
+					id: 'jpsap29702'
+				},
+				{
+					name: 'HOTEL MYSTAYS Sapporo Nakajima Park',
+					address: '1-20 Nishi 1-chome, Minami 14-jo, Chuo-ku, Sapporo-shi, Hokkaido 064-0914 Japan',
+					img: 'HOTEL-MYSTAYS-Sapporo-Nakajima-Park.jpg',
+					id: 'jpsap27590'
+				},
+				{
+					name: 'HOTEL MYSTAYS Sapporo Nakajima Park Annex',
+					address: '4-3 Nishi 1-chome, Minami 11-jo, Chuo-ku, Sapporo-shi, Hokkaido 064-0811 Japan',
+					img: 'HOTEL-MYSTAYS-Sapporo-Nakajima-Park-Annex.jpg',
+					id: 'jphok30695'
+				},
+				{
+					name: 'HOTEL MYSTAYS Sapporo Station',
+					address: '15 Nishi 4-chome, Kita 8-jo, Kita-ku, Sapporo-shi, Hokkaido 060-0808 Japan',
+					img: 'HOTEL-MYSTAYS-Sapporo-Station.jpg',
+					id: 'jpsap29772'
+				},
+				{
+					name: 'ART HOTEL ASAHIKAWA',
+					address: '6 7-jo-dori, Asahikawa-shi, Hokkaido 070-0037 Japan',
+					img: 'ART-HOTEL-ASAHIKAWA.jpg',
+					id: 'jphok28822'
+				},
+				{
+					name: 'Hotel Nord Otaru',
+					address: '1-4-16 Ironai, Otaru-shi, Hokkaido 047-0031 Japan',
+					img: 'Hotel-Nord-Otaru.jpg',
+					id: 'jpota28373'
+				},
+				{
+					name: 'Hotel Sonia Otaru',
+					address: '1-4-20 Ironai, Otaru-shi, Hokkaido 047-0031 Japan',
+					img: 'Hotel-Sonia-Otaru.jpg',
+					id: 'jpota29771'
+				},
+				{
+					name: 'Hakodate Kokusai Hotel',
+					address: '5-10 Ote-machi, Hakodate-shi, Hokkaido 040-0064 Japan',
+					img: 'Hakodate-Kokusai-Hotel.jpg',
+					id: 'jphok29664'
+				}
+			]
+		},
+		'aomori':{
+			city: 'Aomori',
+			id: 'aomori',
+			hotels: [
+				{
+					name: 'ART HOTEL HIROSAKI CITY',
+					address: '1-1-2 Omachi, Hirosaki-shi, Aomori 036-8004 Japan',
+					img: 'ART-HOTEL-HIROSAKI-CITY.jpg',
+					id: 'jpaom28827'
+				}
+			]
+		},
+		'yamanashi':{
+			city: 'Yamanashi',
+			id: 'yamanashi',
+			hotels: [
+				{
+					name: 'HOTEL MYSTAYS Fuji',
+					address: '2654 Arakura, Fujiyoshida-shi, Yamanashi 403-0011 Japan',
+					img: 'HOTEL-MYSTAYS-Fuji.jpg',
+					id: 'jpyam29701'
+				}
+			]
+		},
+		'nagoya':{
+			city: 'Nagoya',
+			id: 'nagoya',
+			hotels: [
+				{
+					name: 'HOTEL MYSTAYS Nagoya Sakae',
+					address: '2-23-22 Higashisakura, Naka-ku, Nagoya-shi, Aichi 460-0005 Japan',
+					img: 'HOTEL-MYSTAYS-Nagoya-Sakae.JPG',
+					id: 'jpnag26321'
+				}
+			]
+		},
+		'niigata':{
+			city: 'Niigata',
+			id: 'niigata',
+			hotels: [
+				{
+					name: 'ART HOTEL NIIGATA STATION',
+					address: '1-1 Sasaguchi, Chuo-ku, Niigata-shi, Niigata 950-0911 Japan',
+					img: 'ARTHOTEL-NIIGATA-STATION.jpg',
+					id: 'jpchu29824'
+				},
+				{
+					name: 'ART HOTEL JOETSU',
+					address: '5-1-11 Hon-cho, Joetsu-shi, Niigata 943-0832 Japan',
+					img: 'ART-HOTEL-JOETSU.jpg',
+					id: 'jpjoe29825'
+				}
+			]
+		},
+		'kagawa':{
+			city: 'Kagawa',
+			id: 'kagawa',
+			hotels: [
+				{
+					name: 'Resort Hotel Olivean Shodoshima',
+					address: '63-1 Yakatazaki, Tonosho, Kagawa 761-4142 Japan',
+					img: 'Resort-Hotel-Olivean-Shodoshima.jpg',
+					id: 'jpton28829'
+				}
+			]
+		},
+		'ehime':{
+			city: 'Ehime',
+			id: 'ehime',
+			hotels: [
+				{
+					name: 'HOTEL MYSTAYS Matsuyama',
+					address: '1-10-10 Otemachi, Matsuyama-shi, Ehime 790-1167 Japan',
+					img: 'HOTEL-MYSTAYS-Matsuyama.jpg',
+					id: 'jpehi31183'
+				}
+			]
+		},
+		'fukuoka':{
+			city: 'Fukuoka',
+			id: 'fukuoka',
+			hotels: [
+				{
+					name: 'HOTEL MYSTAYS Fukuoka Tenjin Minami',
+					address: '3-14-20 Haruyoshi, Chuo-ku, Fukuoka-shi, Fukuoka 810-0003 Japan',
+					img: 'HOTEL-MYSTAYS-Fukuoka-Tenjin-Minami.jpg',
+					id: 'jpfuk26314'
+				},
+				{
+					name: 'HOTEL MYSTAYS Fukuoka Tenjin',
+					address: '3-5-7 Tenjin, Chuo-ku, Fukuoka-shi, Fukuoka 810-0001 Japan',
+					img: 'HOTEL-MYSTAYS-Fukuoka-Tenjin.JPG',
+					id: 'jpfuk26326'
+				}
+			]
+		},
+		'oita':{
+			city: 'Oita',
+			id: 'oita',
+			hotels: [
+				{
+					name: 'HOTEL MYSTAYS Oita',
+					address: '1-32 Niagemachi,Oita-shi, Oita 870-0046 Japan',
+					img: 'HOTEL-MYSTAYS-Oita.jpg',
+					id: 'jpoit29773'
+				},
+				{
+					name: 'Beppu Kamenoi Hotel',
+					address: '5-17 Chuo-cho, Beppu-shi, Oita 874-0936 Japan',
+					img: 'Beppu-Kamenoi-Hotel.jpg',
+					id: 'jpbep27802'
+				}
+			]
+		},
+		'okinawa':{
+			city: 'Okinawa',
+			id: 'okinawa',
+			hotels: [
+				{
+					name: 'ART HOTEL ISHIGAKIJIMA',
+					address: '559 Ohkawa, Ishigaki-shi, Okinawa 907-0022 Japan',
+					img: 'ART-HOTEL-ISHIGAKIJIMA.JPG',
+					id: 'jpoki29665'
+				},
+				{
+					name: 'Fusaki Resort Village (Ishigaki-jima)',
+					address: '1625 Arakawa, Ishigaki-shi, Okinawa 907-0024 Japan',
+					img: 'Fusaki-Resort-Village-(Ishigaki-jima).jpg',
+					id: 'jpoki28825'
+				}
+			]
+		}
+	};
+
+	window.miscellaneous = {
+		hotelSelectPlaceholder: 'Please select a Hotel',
+		roomLabel: 'Room',
+		adultLabel: 'Adults',
+		childrenLabel: 'Children',
+		sliderSelectButton: 'Select Hotel',
+		hotelSingular: 'Hotel',
+		hotelPlural: 'Hotels',
+		deleteRoom: 'Delete this room',
+		membersOnly: 'Members Only'
+	};
+})(window);

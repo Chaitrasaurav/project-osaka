d5d67913cce864736cfd569290461a1b683065d3
Anubhav VISHWAKARMA <anubhav.vishwakarma@amadeus.com>
2017-10-01 13:58:46 +0530

    conflict resolving

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,34 +1,38 @@
+$(window).ready(function(){
+	var countryCode = 'en';
 
+	window.data.main.map(function(value){
+		$('[data-sheet=' + value.Item + ']').text(value[countryCode]);			
+	});
 
-$('.accommodation_slider').slick({
-	lazyLoad: 'ondemand',
-	slidesToShow: 3,
-	speed: 2000,
-	slidesToScroll: 3,
-	dots: true,
-	infinite: false,
-	responsive: [	   
-	{
-	  breakpoint: 900,
-	  settings: {
-	    slidesToShow: 2,
-	    slidesToScroll: 2
-	  }
-	},
-	{
-	  breakpoint: 600,
-	  settings: {
-	    slidesToShow: 1,
-	    slidesToScroll: 1,
-	  }
-	}
-	]
-});
+	$('.js-accomodation').slick({
+		lazyLoad: 'ondemand',
+		slidesToShow: 3,
+		speed: 2000,
+		slidesToScroll: 4,
+		dots: true,
+		responsive: [	   
+		{
+		  breakpoint: 900,
+		  settings: {
+		    slidesToShow: 2,
+		    slidesToScroll: 2
+		  }
+		},
+		{
+		  breakpoint: 600,
+		  settings: {
+		    slidesToShow: 1,
+		    slidesToScroll: 1,
+		  }
+		}
+		]
+	});
 
+	$('.slick-arrow').appendTo('.slick-dots');
+	$('.slick-arrow').addClass('icon arrow');
 
-$('.slick-arrow').appendTo('.slick-dots');
-$('.slick-arrow').addClass('icon arrow');
-
-$('.header_cta-dropdown').on('click', function(){
-	$('.header_language').toggleClass('active');
+	$('.header_cta-dropdown').on('click', function(){
+		$('.header_language').toggleClass('active');
+	});
 });

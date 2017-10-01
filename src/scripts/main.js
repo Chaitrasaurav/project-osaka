d8a9c2bfdf8aa690321bd08c5e12145bb89ccea8
Chaitra <chaitranaiksaurav@gmail.com>
2017-10-01 16:30:33 +0530

    created photo gallery

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,11 +1,16 @@
 $(window).ready(function(){
 	var countryCode = 'en';
 
-	window.data.main.map(function(value){
-		$('[data-sheet=' + value.Item + ']').text(value[countryCode]);			
-	});
+	//window.data.main.map(function(value){
+	//	$('[data-sheet=' + value.Item + ']').text(value[countryCode]);			
+	//});
 
-	$('.js-accomodation').slick({
+
+});
+
+
+
+$('.js-accomodation').slick({
 		lazyLoad: 'ondemand',
 		slidesToShow: 3,
 		speed: 2000,
@@ -35,4 +40,39 @@ $(window).ready(function(){
 	$('.header_cta-dropdown').on('click', function(){
 		$('.header_language').toggleClass('active');
 	});
-});
+
+	var $grid = $('.grid').masonry({
+		// options...
+		itemSelector: '.grid-item',
+		isFitWidth: true,
+		columnWidth: 1
+	});
+
+	$('.js-tabs-select').on('change', function(e) {
+
+		
+		var tabId = $(this).val();
+
+		$('ul.tabs li').removeClass('current');
+		$('.tab-content').removeClass('current');
+
+		$('ul.js-tabs li[data-tab="'+ tabId +'"]').addClass('current');
+		$("#"+tabId).addClass('current');
+		$('.js-accomodation').slick('refresh');
+
+	});
+
+	$('ul.js-tabs li').click(function(){
+
+		var tabId = $(this).attr('data-tab');
+
+		$('ul.js-tabs li').removeClass('current');
+		$('.tab-content').removeClass('current');
+
+		$('.js-tabs-select').val(tabId).trigger('change');
+		$(this).addClass('current');
+		$("#"+tabId).addClass('current');
+		$('.js-accomodation').slick('refresh');
+
+	});
+
